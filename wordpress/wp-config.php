3f62ea6e009bfbe5ead41ff318c7e05a5b51c583
Brian McGehee <doc@tavian.com>
2018-03-26 12:54:45 -0700

    fix3

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -2,7 +2,7 @@
 define('DB_NAME', 'wordpress');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_HOST', "127.0.0.1:3306");
+define('DB_HOST', "127.0.0.1");
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
